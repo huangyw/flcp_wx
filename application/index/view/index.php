699ab39c8799187f4168c7a16b26c8081e9714b3
huangyw <hywkevin@yeah.net>
2018-04-23 16:48:58 +0800

    Initial commit

---
+++ application/index/view/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+<head lang="zh-cn">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<link rel="stylesheet" href="css/myweui.min.css">
+	<script type="text/javascript" src="js/myweui.min.js"></script>
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	<script type="text/javascript" src="js/do.js"></script>
+	<script type="text/javascript">
+        $(document).ready(function(){
+            _do.a();
+        });
+	</script>
+</head>
+<body>
+<div id="container" class="container">
+	<div class="weui-article">
+		<img class="" src="image/feed.jpg">
+		<h1>第001期合买服务</h1>
+		<p>最低购买1元，只要加满进度条就可以下单了！</p>
+		<div class="weui-progress">
+			<div class="weui-progress__bar">
+				<div class="weui-progress__inner-bar" style="width: 80%"></div>
+			</div>
+			<span class="weui-badge">80%</span>
+		</div>
+		<h1></h1>
+		<div class="weui-grids">
+			<a class="weui-grid" href="">
+				<div class="weui-grid__label" align="center">目标金额/元</div>
+				<p class="weui-grid__label">840</p></a>
+			<a class="weui-grid" href="">
+				<div class="weui-grid__label" align="center">当前金额/元</div>
+				<p class="weui-grid__label">664</p></a>
+			<a class="weui-grid" href="">
+				<div class="weui-grid__label" align="center">参与人数/人</div>
+				<p class="weui-grid__label">56</p></a>
+		</div>
+		<h1></h1>
+		<h2 style="background-color: #ffba41">团购介绍</h2>
+		<p>
+			在中国，每一个初生妈妈都是幸福的，为迎接一个新的生命，付出独一无二的爱。但往往并不是每一个初生妈妈都懂得为宝宝的身体健康保驾护航，幸福妈妈哺乳日记将坚持每两天更新一次课程，同时让大家团购课程，只要课程团满，即可提供团购价格相应的一对一咨询服务，赶紧填满上面的进度条吧！</p>
+		<a class="weui-btn weui-btn_primary" href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx94873c62b20047e1&redirect_uri=http://1n510107x3.51mypc.cn/wx/info.php&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect">我要参团</a>
+	</div>
+</div>
+
+<div class="weui-footer weui-footer_fixed-bottom">
+	<p class="weui-footer__text">Copyright © 2017- HywKevin</p>
+</div>
+</body>
+</html>
+
